4d3d0f8a04511061af794bbb8b825cc37f4d9b3f
luncheon <takeuck+github@gmail.com>
2020-03-05 17:14:44 +0900

    fix: escape '>' in html

--- src/escapeHtml.ts
+++ src/escapeHtml.ts
@@ -1,1 +1,1 @@
-export const escapeHtml = (s: string) => s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;')
+export const escapeHtml = (s: string) => s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')
