7b4adab74e88c9f905d9611151ba330488c565bd
luncheon <takeuck+github@gmail.com>
2020-04-05 10:21:02 +0900

    refactor: cut out `renderSectionContent()` into renderSectionContent.ts.

--- src/render.ts
+++ src/render.ts
@@ -1,20 +1,19 @@
 /* eslint @typescript-eslint/camelcase: ["error", { allow: ["^WIP_"] }] */
 import { DXF_COLOR_HSL } from '@dxfom/color/hsl'
-import { createDxfFileString, Dxf, DxfRecordReadonly, getGroupCodeValue, getGroupCodeValues } from '@dxfom/dxf'
+import { createDxfFileString, DxfReadonly } from '@dxfom/dxf'
 import { createSvgString } from '@dxfom/svg'
-import { parseDxfTextContent } from '@dxfom/text'
 import { html, render as _render } from 'lit-html'
 import { repeat } from 'lit-html/directives/repeat'
 import 'svg-pan-zoom-container'
-import { renderDataTable } from './renderDataTable'
-
-const identity = <T>(x: T) => x
+import { renderSectionContent } from './renderSectionContent'
 
 let state = {
   filename: '',
-  dxf: {} as Dxf,
-  activeSectionName: '' as '' | 'PREVIEW' | 'DXF' | 'JSON' | keyof Dxf,
+  dxf: {} as DxfReadonly,
+  activeSectionName: '' as '' | 'PREVIEW' | 'DXF' | 'JSON' | keyof DxfReadonly,
 }
+
+const identity = <T>(x: T) => x
 
 const onChangeActiveSection = (event: Event) => render({
   ...state,
@@ -46,71 +45,12 @@ const renderNavigation = (parentElement: HTMLElement) =>
     parentElement,
   )
 
-const collectCodes = (records: readonly DxfRecordReadonly[]) => {
-  const codes: number[] = []
-  for (const record of records) {
-    for (const [code] of record) {
-      codes.includes(code) || codes.push(code)
-    }
-  }
-  return codes.sort((a, b) => a - b)
-}
-
-const renderDefaultSectionContent = (tableContainerElement: HTMLElement, filterInputElement: HTMLInputElement, section: Dxf['ENTITIES']) => {
-  if (Array.isArray(section)) {
-    renderDataTable(
-      tableContainerElement,
-      filterInputElement,
-      collectCodes(section),
-      section,
-      code => record => record.filter(([n]) => n === code).map(([, value]) => value).join('\n'),
-      code => code !== 1 ? undefined : record => {
-        const value = getGroupCodeValue(record, 1)
-        if (!value) {
-          return
-        }
-        switch (getGroupCodeValue(record, 0)) {
-          case 'TEXT': {
-            const text = parseDxfTextContent(value).map(({ text }) => text).join('')
-            return text === value ? undefined : ({ type: 'info', message: text })
-          }
-          case 'MTEXT': {
-            const formattedText = getGroupCodeValues(record, 3).join('') + value
-            // todo
-            const text = formattedText
-            return text === value ? undefined : ({ type: 'info', message: text })
-          }
-        }
-      },
-    )
-  }
-}
-
-const renderHeaderOrClassesSectionContent = (
-  parentElement: HTMLElement,
-  section: Dxf['HEADER' | 'CLASSES'],
-  nameGroupCode: number,
-) => {
-  if (section) {
-    const filterInputElement = parentElement.appendChild(document.createElement('input'))
-    const tableContainerElement = parentElement.appendChild(document.createElement('article'))
-    tableContainerElement.style.height = `calc(100% - ${ filterInputElement.offsetHeight + 4 }px - 1em)`
-    renderDataTable(
-      tableContainerElement,
-      filterInputElement,
-      [nameGroupCode, ...collectCodes(Object.values(section) as DxfRecordReadonly[])],
-      Object.entries(section),
-      code => code === nameGroupCode ? ([name]) => name : ([name]) => getGroupCodeValue(section[name], code),
-    )
-  }
-}
-
 const resolveColorIndex = (index: number | string | undefined) => {
   const [h, s, l] = DXF_COLOR_HSL[index as number & string] ?? [0, 0, 50]
   return `hsl(${h},${s}%,${l * .8 + 20}%)`
 }
 
-const renderSectionContent = (parentElement: HTMLElement) => {
+const renderTabContent = (parentElement: HTMLElement) => {
   parentElement.innerHTML = ''
   if (!state.activeSectionName) {
     return
@@ -140,66 +80,8 @@ const renderSectionContent = (parentElement: HTMLElement) => {
       return parentElement.innerHTML = `<textarea readonly style="width: 100%; height: 100%; resize: none;">${createDxfFileString(state.dxf)}</textarea>`
     case 'JSON':
       return parentElement.innerHTML = `<textarea readonly style="width: 100%; height: 100%; resize: none;">${JSON.stringify(state.dxf, undefined, 2)}</textarea>`
-    case 'HEADER':
-      return renderHeaderOrClassesSectionContent(parentElement, dxf[activeSectionName], 9)
-    case 'CLASSES':
-      return renderHeaderOrClassesSectionContent(parentElement, dxf[activeSectionName], 1)
-    case 'OBJECTS':
-      if (dxf[activeSectionName]) {
-        const sectionElement = parentElement.appendChild(document.createElement('section'))
-        for (const record of dxf[activeSectionName]!) {
-          sectionElement.appendChild(document.createElement('h3')).textContent = getGroupCodeValue(record, 0)!
-          const table = sectionElement.appendChild(document.createElement('article')).appendChild(document.createElement('table'))
-          for (const [code, value] of record) {
-            const row = table.insertRow()
-            row.insertCell().textContent = code as number & string
-            row.insertCell().textContent = value
-          }
-        }
-      }
-      break
-    case 'TABLES':
-    case 'BLOCKS':
-      if (dxf[activeSectionName]) {
-        const sectionElement = parentElement.appendChild(document.createElement('section'))
-        const filterContainerElement = sectionElement.appendChild(document.createElement('div'))
-        filterContainerElement.className = 'filter-input-container'
-        const filterInputElement = filterContainerElement.appendChild(document.createElement('input'))
-        sectionElement.style.paddingTop = `calc(${filterInputElement.offsetHeight + 4}px + 1em)`
-        for (const name in dxf[activeSectionName]) {
-          sectionElement.appendChild(document.createElement('h3')).textContent = name
-          const tableContainerElement = sectionElement.appendChild(document.createElement('article'))
-          tableContainerElement.tabIndex = 0
-          tableContainerElement.style.display = 'flex'
-          tableContainerElement.style.maxHeight = '30vh'
-          renderDefaultSectionContent(tableContainerElement, filterInputElement, dxf[activeSectionName]![name])
-        }
-      }
-      break
-    case 'ACDSDATA':
-      if (dxf[activeSectionName]) {
-        const sectionElement = parentElement.appendChild(document.createElement('section'))
-        const filterContainerElement = sectionElement.appendChild(document.createElement('div'))
-        filterContainerElement.className = 'filter-input-container'
-        const filterInputElement = filterContainerElement.appendChild(document.createElement('input'))
-        sectionElement.style.paddingTop = `calc(${filterInputElement.offsetHeight + 4}px + 1em)`
-        for (const records of dxf[activeSectionName]!) {
-          const tableContainerElement = sectionElement.appendChild(document.createElement('article'))
-          tableContainerElement.tabIndex = 0
-          tableContainerElement.style.display = 'flex'
-          tableContainerElement.style.maxHeight = '30vh'
-          renderDefaultSectionContent(tableContainerElement, filterInputElement, records)
-        }
-      }
-      break
     default:
-      if (dxf[activeSectionName]) {
-        const filterInputElement = parentElement.appendChild(document.createElement('input'))
-        const tableContainerElement = parentElement.appendChild(document.createElement('article'))
-        tableContainerElement.style.height = `calc(100% - ${ filterInputElement.offsetHeight + 4 }px - 1em)`
-        renderDefaultSectionContent(tableContainerElement, filterInputElement, dxf[activeSectionName])
-      }
-      break
+      return renderSectionContent(parentElement, dxf, activeSectionName)
   }
 }
 
@@ -209,7 +91,7 @@ const contentElement = document.getElementsByTagName('main')[0]
 export function render(_state: typeof state) {
   state = _state
   renderNavigation(navElement)
-  renderSectionContent(contentElement)
+  renderTabContent(contentElement)
   const start = performance.now()
   setTimeout(() => console.log(`Rendering takes ${performance.now() - start} [ms]`))
 }
---
+++ src/renderSectionContent.ts
@@ -0,0 +1,128 @@
+import { DxfReadonly, DxfRecordReadonly, getGroupCodeValue, getGroupCodeValues } from "@dxfom/dxf"
+import { parseDxfTextContent } from '@dxfom/text'
+import { renderDataTable } from "./renderDataTable"
+
+const collectCodes = (records: readonly DxfRecordReadonly[]) => {
+  const codes: number[] = []
+  for (const record of records) {
+    for (const [code] of record) {
+      codes.includes(code) || codes.push(code)
+    }
+  }
+  return codes.sort((a, b) => a - b)
+}
+
+const renderHeaderOrClassesSectionContent = (
+  parentElement: HTMLElement,
+  section: DxfReadonly['HEADER' | 'CLASSES'],
+  nameGroupCode: number,
+) => {
+  if (section) {
+    const filterInputElement = parentElement.appendChild(document.createElement('input'))
+    const tableContainerElement = parentElement.appendChild(document.createElement('article'))
+    tableContainerElement.style.height = `calc(100% - ${ filterInputElement.offsetHeight + 4 }px - 1em)`
+    renderDataTable(
+      tableContainerElement,
+      filterInputElement,
+      [nameGroupCode, ...collectCodes(Object.values(section) as DxfRecordReadonly[])],
+      Object.entries(section),
+      code => code === nameGroupCode ? ([name]) => name : ([name]) => getGroupCodeValue(section[name], code),
+    )
+  }
+}
+
+const renderDefaultSectionContent = (tableContainerElement: HTMLElement, filterInputElement: HTMLInputElement, records: readonly DxfRecordReadonly[] | undefined) => {
+  if (!Array.isArray(records)) {
+    return
+  }
+  renderDataTable<DxfRecordReadonly, number>(
+    tableContainerElement,
+    filterInputElement,
+    collectCodes(records),
+    records,
+    code => record => record.filter(([n]) => n === code).map(([, value]) => value).join('\n'),
+    code => code !== 1 ? undefined : record => {
+      const value = getGroupCodeValue(record, 1)
+      if (!value) {
+        return
+      }
+      switch (getGroupCodeValue(record, 0)) {
+        case 'TEXT': {
+          const text = parseDxfTextContent(value).map(({ text }) => text).join('')
+          return text === value ? undefined : ({ type: 'info', message: text })
+        }
+        case 'MTEXT': {
+          const formattedText = getGroupCodeValues(record, 3).join('') + value
+          // todo
+          const text = formattedText
+          return text === value ? undefined : ({ type: 'info', message: text })
+        }
+      }
+    },
+  )
+}
+
+export const renderSectionContent = (parentElement: HTMLElement, dxf: DxfReadonly, sectionName: keyof DxfReadonly) => {
+  switch (sectionName) {
+    case 'HEADER':
+      return renderHeaderOrClassesSectionContent(parentElement, dxf[sectionName], 9)
+    case 'CLASSES':
+      return renderHeaderOrClassesSectionContent(parentElement, dxf[sectionName], 1)
+    case 'OBJECTS':
+      if (dxf[sectionName]) {
+        const sectionElement = parentElement.appendChild(document.createElement('section'))
+        for (const record of dxf[sectionName]!) {
+          sectionElement.appendChild(document.createElement('h3')).textContent = getGroupCodeValue(record, 0)!
+          const table = sectionElement.appendChild(document.createElement('article')).appendChild(document.createElement('table'))
+          for (const [code, value] of record) {
+            const row = table.insertRow()
+            row.insertCell().textContent = code as number & string
+            row.insertCell().textContent = value
+          }
+        }
+      }
+      break
+    case 'TABLES':
+    case 'BLOCKS':
+      if (dxf[sectionName]) {
+        const sectionElement = parentElement.appendChild(document.createElement('section'))
+        const filterContainerElement = sectionElement.appendChild(document.createElement('div'))
+        filterContainerElement.className = 'filter-input-container'
+        const filterInputElement = filterContainerElement.appendChild(document.createElement('input'))
+        sectionElement.style.paddingTop = `calc(${filterInputElement.offsetHeight + 4}px + 1em)`
+        for (const name in dxf[sectionName]) {
+          sectionElement.appendChild(document.createElement('h3')).textContent = name
+          const tableContainerElement = sectionElement.appendChild(document.createElement('article'))
+          tableContainerElement.tabIndex = 0
+          tableContainerElement.style.display = 'flex'
+          tableContainerElement.style.maxHeight = '30vh'
+          renderDefaultSectionContent(tableContainerElement, filterInputElement, dxf[sectionName]![name])
+        }
+      }
+      break
+    case 'ACDSDATA':
+      if (dxf[sectionName]) {
+        const sectionElement = parentElement.appendChild(document.createElement('section'))
+        const filterContainerElement = sectionElement.appendChild(document.createElement('div'))
+        filterContainerElement.className = 'filter-input-container'
+        const filterInputElement = filterContainerElement.appendChild(document.createElement('input'))
+        sectionElement.style.paddingTop = `calc(${filterInputElement.offsetHeight + 4}px + 1em)`
+        for (const records of dxf[sectionName]!) {
+          const tableContainerElement = sectionElement.appendChild(document.createElement('article'))
+          tableContainerElement.tabIndex = 0
+          tableContainerElement.style.display = 'flex'
+          tableContainerElement.style.maxHeight = '30vh'
+          renderDefaultSectionContent(tableContainerElement, filterInputElement, records)
+        }
+      }
+      break
+    default:
+      if (dxf[sectionName]) {
+        const filterInputElement = parentElement.appendChild(document.createElement('input'))
+        const tableContainerElement = parentElement.appendChild(document.createElement('article'))
+        tableContainerElement.style.height = `calc(100% - ${ filterInputElement.offsetHeight + 4 }px - 1em)`
+        renderDefaultSectionContent(tableContainerElement, filterInputElement, dxf[sectionName])
+      }
+      break
+  }
+}
