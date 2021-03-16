b82ee84286bc4bdb7b4c00a0e4deb31fbc45f39d
luncheon <takeuck+github@gmail.com>
2021-03-16 23:00:31 +0900

    change bundler (rollup -> esbuild)

---
+++ build.mjs
@@ -0,0 +1,30 @@
+import esbuild from 'esbuild'
+import babel from 'esbuild-plugin-babel'
+import fs from 'fs'
+
+fs.mkdirSync('docs', { recursive: true })
+fs.copyFileSync('src/index.html', 'docs/index.html')
+fs.copyFileSync('src/favicon.png', 'docs/favicon.png')
+
+const options = {
+  entryPoints: ['src/index.tsx'],
+  outdir: 'docs',
+  resolveExtensions: ['.mjs', '.js', '.ts', '.tsx'],
+  bundle: true,
+  minify: true,
+  format: 'esm',
+  target: 'es2020',
+  logLevel: 'info',
+  plugins: [
+    babel({
+      filter: /\.tsx$/,
+      config: { presets: ['@babel/preset-typescript', 'babel-preset-solid'] },
+    }),
+  ],
+}
+
+if (process.argv.includes('--serve')) {
+  esbuild.serve({ servedir: 'docs' }, options).then(console.log('http://localhost:8000'))
+} else {
+  esbuild.build(options)
+}
